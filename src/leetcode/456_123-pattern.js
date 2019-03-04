fba67b80a711e1320a6f1764065eba9e32746378
Tri Le <tri.le@oddle.me>
2019-03-04 10:24:32 +0800

    LC 456

---
+++ src/leetcode/456_123-pattern.js
@@ -0,0 +1,33 @@
+import Stack from '../Stack'
+
+const find132pattern = nums => {
+  if (nums.length < 3) return false
+
+  const s = new Stack()
+
+  const minBeforeCur = [Infinity]
+
+  for (let i = 1; i < nums.length; i += 1) {
+    minBeforeCur.push(Math.min(minBeforeCur[i - 1], nums[i - 1]))
+  }
+
+  for (let i = 0; i < nums.length; i += 1) {
+    const val = nums[i]
+    let top = s.peek()
+
+    while (top && top.val <= val) {
+      s.pop()
+      top = s.peek()
+    }
+
+    if (top && top.val > val && top.min < val) {
+      return true
+    }
+
+    s.push({ val, min: minBeforeCur[i] })
+  }
+
+  return false
+}
+
+export default find132pattern
